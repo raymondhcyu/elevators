1389c24acce49f1ce53c825854a540e029441040
Raymond <yuraymond96@gmail.com>
2019-11-02 17:00:18 -0700

    Added message packet int conversion to int

--- CPEN333Assignment1/Dispatcher.cpp
+++ CPEN333Assignment1/Dispatcher.cpp
@@ -7,42 +7,43 @@ TheMonitorTwo elevatorTwoMonitor;
 CRendezvous r1("CreationRendezvous", 4); // sync creation of 4x processes
 
 char exitProgram[] = { 'e', 'e', '\0' };
-char upTwo[] = { 'u', '2', '\0' };
 
 struct IODispatch {
 	char inputs[3];
 };
 
 IODispatch pipeIOData;
-int messagePacket[5] = {};
+int messagePacket[5] = {}; // message packet interpretted from IO
+int elevatorOneMessage = 0; // message packet to be sent to E1 mailbox
 
 CSemaphore messagePacketProducer("MessagePacketProducer", 0);
-CSemaphore messagePacketConsumer("MessagePacketProducer", 1);
+CSemaphore messagePacketConsumer("MessagePacketConsumer", 1);
 
 UINT __stdcall Thread1(void* args) {
 
 	CTypedPipe <IODispatch> PipeIODispatch("PipelineIODispatch", 100); // room for 100 data
-	
+
 	while (1) {
 		if (PipeIODispatch.TestForData() >= sizeof(pipeIOData) / 3) { // size of struct is 3
 			PipeIODispatch.Read(&pipeIOData);
 
 			std::cout << "Received " << pipeIOData.inputs << endl;
-
 			std::unordered_map<char, int> commandReference { {'u', 2}, {'d', 1} }; // GCOM magic
 
+			messagePacketConsumer.Wait();
 			messagePacket[0] = 1;
 			messagePacket[1] = commandReference[pipeIOData.inputs[0]];
 			messagePacket[2] = 1;
 			messagePacket[3] = 0;
-			messagePacket[4] = atoi(&pipeIOData.inputs[1]);
+			messagePacket[4] = atoi(&pipeIOData.inputs[1]); // convert character to int
+			messagePacketProducer.Signal();
 
-			//messagePacketConsumer.Wait();
-			//messagePacketProducer.Signal();
+			console.Wait();
 			cout << "Message packet content is ";
 			for (auto& mpData : messagePacket) // GCOM magic
 				cout << mpData;
 			cout << endl;
+			console.Signal();
 			break;
 		}
 	}
@@ -50,22 +51,19 @@ UINT __stdcall Thread1(void* args) {
 }
 
 UINT __stdcall Thread2(void* args) {
+	messagePacketProducer.Wait();
+	// Convert message packet int array to int
+	for (int i = 0; i < 5; i++) {
+		elevatorOneMessage *= 10;
+		elevatorOneMessage += messagePacket[i];
+	}
+	messagePacketConsumer.Signal();
 
-	//while (1) {
-	//	if (strcmp(pipeIOData.inputs, upTwo) == 0) {
-	//		console.Wait();
-	//		cout << "Dispatcher read " << pipeIOData.inputs << " from IO..." << endl;
-	//		console.Signal();
-	//		break;
-	//	}
-	//	else if (strcmp(pipeIOData.inputs, exitProgram) == 0) {
-	//		console.Wait();
-	//		cout << "Program exitting..." << endl;
-	//		console.Signal();
-	//		break;
-	//	}
-	//}
+	// *** Need separate semaphores for elevator one message to send to elevator mailbox
 
+	console.Wait();
+	cout << "Elevator one message is: " << elevatorOneMessage << endl;
+	console.Signal();
 	return 0;
 }
 
