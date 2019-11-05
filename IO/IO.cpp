9d1618b472e7ba623bd3afd2ac6df163f75616d3
jasonthegrump <46975978+jasonrleb@users.noreply.github.com>
2019-11-05 11:32:14 -0800

    Add files via upload

--- IO/IO.cpp
+++ IO/IO.cpp
@@ -9,6 +9,11 @@ TheMonitorOne E1Monitor;
 TheMonitorTwo E2Monitor;
 CRendezvous r1("CreationRendezvous", 4); // sync creation of 4x processes
 
+CSemaphore inputB4output1("inputB4output1", 0);
+CSemaphore inputB4output2("inputB4output2", 1);
+
+HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE); //to change console colors etc 
+
 struct IODispatch {
 	char inputs[3];
 };
@@ -17,7 +22,48 @@ int E1Status[5] = {};
 
 void dispatchPipeline(char* inputs);
 
+//function to get filtered user input from keyboard
+void getUserInput(char* input); 
+
+//Animation function
+void animateElevator(int* status);
+
+
 UINT __stdcall UpdateDisplay(void* args) {
+
+	//create layout for animation
+	console.Wait();
+
+	//Build elevator shafts
+	for (int i = 0; i <= 10; i++) {
+			
+			MOVE_CURSOR(ANIMATION_TOPLEFT_X, ANIMATION_TOPLEFT_Y + i);
+			cout << "|";
+			MOVE_CURSOR(ANIMATION_TOPLEFT_X + 8, ANIMATION_TOPLEFT_Y + i);
+			cout << "|";
+			MOVE_CURSOR(ANIMATION_TOPLEFT_X + 16, ANIMATION_TOPLEFT_Y + i);
+			cout << "|";
+
+		}
+
+	//Build floor numbers
+	for (int i = 0; i <= 10; i++) {
+
+		MOVE_CURSOR(ANIMATION_TOPLEFT_X-5, ANIMATION_TOPLEFT_Y + i);
+		cout << "F" << 10 - i;
+
+	}
+
+	//Build Labels
+	MOVE_CURSOR(ANIMATION_TOPLEFT_X + 4, ANIMATION_TOPLEFT_Y - 1);
+	cout << "E1";
+	MOVE_CURSOR(ANIMATION_TOPLEFT_X + 12, ANIMATION_TOPLEFT_Y - 1);
+	cout << "E2";
+
+
+	console.Signal();
+
+
 	while (1) {
 		// Convert monitor update int to int array for processing
 		int E1Update = E1Monitor.getInfoIO(); // wait for data?
@@ -27,8 +73,6 @@ UINT __stdcall UpdateDisplay(void* args) {
 			E1Update /= 10;
 		}
 
-		// @ JASON DO THIS ***********************************************************
-		// Add animations here in this thread
 		console.Wait();
 		if (E1Status[0] == 1) { // 12104
 			MOVE_CURSOR(0, 2);
@@ -66,44 +110,48 @@ UINT __stdcall UpdateDisplay(void* args) {
 			cout << E1Status[4];
 		}
 
+		animateElevator(E1Status);
+
 		MOVE_CURSOR(24, 4); // hotfix to move cursor back to input
 		console.Signal();
 	}
 	return 0;
 }
 
+UINT __stdcall UserInput(void* args) {
+	while (1) {
+		inputB4output2.Wait();
+		getUserInput((char*) args);
+		inputB4output1.Signal();
+	}
+	return 0;
+}
+
+
 int main() {
 	// Rendezvous to start
 	r1.Wait();
 	cout << "Elevator 1\nDirection\t\tService Status\t\tDoor status\t\tFloor\n" << endl;
+	
+	char input[3] = {};
 
 	CThread t1(UpdateDisplay, ACTIVE, NULL);
+	CThread t2(UserInput, ACTIVE, input);
 
 	while (1) {
-		// Get user input (move to separate function later)
-		char input[3] = {};
 
-		console.Wait();
-		MOVE_CURSOR(0, 4);
-		cout << "Enter a valid command: ";
-		console.Signal();
-		// @ JASON DO THIS ***********************************************************
-		// Take more inputs like -1 -2 +1 +2 ee
-		// Do not accept inputs like 'de' or 'ue'
-		// Display correct character inputs if possible instead of uuuuuuu2
-		while ((input[0] != 'u') && (input[0] != 'd') && (input[0] != 'e')) {
-			input[0] = _getch();
-		}
-		while ((input[1] != '2') && (input[1] != '1') && (input[1] != '0') && (input[1] != 'e')) {
-			input[1] = _getch();
-		}
-
+		inputB4output1.Wait();
 		dispatchPipeline(input);
+		inputB4output2.Signal();
+		
 	}
 
 	t1.WaitForThread();
+	t2.WaitForThread();
+
 	return 0;
 }
+
 
 void dispatchPipeline(char* userInput) {
 	CTypedPipe <IODispatch> PipeIODispatch("PipelineIODispatch", 100);
@@ -112,4 +160,114 @@ void dispatchPipeline(char* userInput) {
 	strcpy(dispatch.inputs, userInput); // copy input to struct, change to receive user input later
 
 	PipeIODispatch.Write(&dispatch);
+}
+
+void getUserInput(char* input) {
+
+	console.Wait();
+	MOVE_CURSOR(0, 4);
+	cout << "Enter a valid command:\t     ";
+	MOVE_CURSOR(0, 4);
+	cout << "Enter a valid command:\t"; //reprint to get curser back to being next to semicolon
+	MOVE_CURSOR(0, 5);
+	cout << "Previous command: " << input[0] << input[1]; //reprint to get curser back to being next to semicolon
+	console.Signal();
+
+	input[0] = input[1] = ' '; //clear old inputs
+
+	while ((input[0] != 'u') && (input[0] != 'd') && (input[0] != 'e') && (input[0] != '1') && (input[0] != '2') && (input[0] != '-') && (input[0] != '+')) {
+		input[0] = _getch();
+	}
+	console.Wait();
+	cout << input[0];
+	console.Signal();
+
+	if ((input[0] == 'u') || (input[0] == 'd') || (input[0] == '1') || (input[0] == '2')) {
+		int floor = 99;
+		while (floor > 9 || floor < 0) { //ALT METHOD: while((input[1] != '0') || (input[1] != '1') || (input[1] != '2') || (input[1] != '3') || .........
+
+			input[1] = _getch();
+
+			if (atoi(&input[1]) == 0) { //any non numeric character will return 0 via atoi which can be confused with floor number 0
+				if (input[1] == '0') {
+					floor = atoi(&input[1]); // convert character to int
+				}
+			}
+			else {
+				floor = atoi(&input[1]); // convert character to int
+			}
+
+		}
+		console.Wait();
+		cout << input[1];
+		console.Signal();
+	}
+
+	if ((input[0] == '+') || (input[0] == '-')) {
+		while ((input[1] != '2') && (input[1] != '1')) {
+			input[1] = _getch();
+		}
+		console.Wait();
+		cout << input[1];
+		console.Signal();
+	}
+
+	if (input[0] == 'e') {
+		while (input[1] != 'e') {
+			input[1] = _getch();
+		}
+		console.Wait();
+		cout << input[1];
+		console.Signal();
+	}
+
+	Sleep(500); //Delay time for user input to display
+}
+
+void animateElevator(int* status) {
+
+	console.Wait();
+
+	//clear previous animation
+		for (int i = 0; i <= 10; i++) {
+			if (status[0] == 1) {
+				MOVE_CURSOR(E1_TOPLEFT_X, ANIMATION_TOPLEFT_Y + i);
+			}
+			if (status[0] == 2) {
+				MOVE_CURSOR(E2_TOPLEFT_X, ANIMATION_TOPLEFT_Y + i);
+
+			}
+			cout << "       ";
+		}
+
+		//Display Elevator
+		if (status[0] == 1) {
+			MOVE_CURSOR(E1_TOPLEFT_X, ANIMATION_TOPLEFT_Y + 10- status[4]);
+		}
+		if (status[0] == 2) {
+			MOVE_CURSOR(E2_TOPLEFT_X, ANIMATION_TOPLEFT_Y + 10- status[4]);
+		}
+
+		if (status[2] == 0) { //if out of service
+			SetConsoleTextAttribute(hConsole, RED);
+			cout << "[  X  ]";
+			SetConsoleTextAttribute(hConsole, WHITE);
+		}
+		if (status[2] == 1) { //if in service: display door open or closed
+			SetConsoleTextAttribute(hConsole, GREEN);
+			if (status[3] == 0) {
+				cout << "[  |  ]";
+			}
+			if (status[3] == 1) {
+				cout << "[|   |]";
+			}
+			SetConsoleTextAttribute(hConsole, WHITE);
+		}
+
+	console.Signal();
+
+
+
+
+
 }
