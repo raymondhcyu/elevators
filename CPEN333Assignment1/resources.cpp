c7835cac12a56bede1031a2f9e5cbfba4b45918a
Raymond <yuraymond96@gmail.com>
2019-11-06 22:51:44 -0800

    Updated monitor 2 for elevator 2

--- CPEN333Assignment1/resources.cpp
+++ CPEN333Assignment1/resources.cpp
@@ -40,6 +40,7 @@ void TheMonitorOne::setInfo(int* data) {
 	ps2->Signal();
 }
 
+// Get info (child IO)
 int TheMonitorOne::getInfoIO(void) {
 	// Convert IO int array message into single int
 	UINT IOmessage = 1; // 1 for elevator1
@@ -58,6 +59,7 @@ int TheMonitorOne::getInfoIO(void) {
 	return IOmessage;
 }
 
+// Get info (child dispatch)
 int TheMonitorOne::getInfoDispatch(void) {
 	// Convert Dispatch int array message into single int
 	UINT dispatchMessage = 1; // 1 for elevator1
@@ -80,39 +82,62 @@ TheMonitorOne::~TheMonitorOne() {}
 
 TheMonitorTwo::TheMonitorTwo() {
 	// Single producer (elevator) double consumer (dispatch and IO)
-	ps1 = new CSemaphore("PS3", 0, 1);
-	ps2 = new CSemaphore("PS4", 0, 1);
-	cs1 = new CSemaphore("CS3", 1, 1);
-	cs2 = new CSemaphore("CS4", 1, 1);
+	ps3 = new CSemaphore("PS3", 0, 1);
+	ps4 = new CSemaphore("PS4", 0, 1);
+	cs3 = new CSemaphore("CS3", 1, 1);
+	cs4 = new CSemaphore("CS4", 1, 1);
 	theDataPool = new CDataPool(string("DataPool2"), sizeof(struct theData));
 	dataPtr = (struct theData*)(theDataPool->LinkDataPool());
 }
 
 // Update floor (producer)
-void TheMonitorTwo::setFloor(int initFloor) {
-	cs1->Wait();
-	cs2->Wait();
-	dataPtr->floor = initFloor;
-	ps1->Signal();
-	ps2->Signal();
-}
-
-// Get floor (child dispatcher)
-int TheMonitorTwo::getFloorDispatch(void) {
-	ps1->Wait();
-	int theFloor = dataPtr->floor; // can't return here else doesn't signal
-	cs1->Signal();
-
-	return theFloor;
+void TheMonitorTwo::setInfo(int* data) {
+	cs3->Wait();
+	cs4->Wait();
+	dataPtr->floor = data[4];
+	dataPtr->doorStatus = data[3];
+	dataPtr->serviceStatus = data[2];
+	dataPtr->direction = data[1];
+	ps3->Signal();
+	ps4->Signal();
 }
 
 // Get floor (child IO)
-int TheMonitorTwo::getFloorIO(void) {
-	ps2->Wait();
-	int theFloor = dataPtr->floor; // can't return here else doesn't signal
-	cs2->Signal();
+int TheMonitorTwo::getInfoIO(void) {
+	// Convert IO int array message into single int
+	UINT IOmessage = 2; // 2 for elevator2
 
-	return theFloor;
+	ps3->Wait();
+	IOmessage *= 10; // 20
+	IOmessage += dataPtr->direction; // ex 21
+	IOmessage *= 10; // ex 220
+	IOmessage += dataPtr->serviceStatus; // ex 221
+	IOmessage *= 10; // ex 2210
+	IOmessage += dataPtr->doorStatus; // ex 2210
+	IOmessage *= 10; // ex 22100
+	IOmessage += dataPtr->floor; // ex 22109
+	cs3->Signal();
+
+	return IOmessage;
+}
+
+// Get floor (child dispatch)
+int TheMonitorTwo::getInfoDispatch(void) {
+	// Convert IO int array message into single int
+	UINT dispatchMessage = 2; // 2 for elevator2
+
+	ps4->Wait();
+	dispatchMessage *= 10; // 20
+	dispatchMessage += dataPtr->direction; // ex 21
+	dispatchMessage *= 10; // ex 220
+	dispatchMessage += dataPtr->serviceStatus; // ex 221
+	dispatchMessage *= 10; // ex 2210
+	dispatchMessage += dataPtr->doorStatus; // ex 2210
+	dispatchMessage *= 10; // ex 22100
+	dispatchMessage += dataPtr->floor; // ex 22109
+	cs4->Signal();
+
+	return dispatchMessage;
 }
 
 TheMonitorTwo::~TheMonitorTwo() {}
